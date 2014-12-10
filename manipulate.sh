00115a1dbde2245bbafc0f5c5865ebc3f56cb88a
lavcraft <lavcraft@gmail.com>
2014-12-11 01:16:53 +0300

    Update manipulate.sh

--- manipulate.sh
+++ manipulate.sh
@@ -5,7 +5,7 @@ cd $(dirname $BASH_SOURCE)
 export WINDOWS_HOST=tolkv@localhost
 export TARGET_LOCAL_DIR=./in
 export TARGET_REMOTE_DIR=~/out
-export TARGET_REMOTE_SCRIPT="~/killmeplease.sh $TARGET_REMOTE_DIR"
+export TARGET_REMOTE_SCRIPT="~/transformmeplease.sh $TARGET_REMOTE_DIR"
 export TARGET_DIR=./iamtransformeddir
 
 ./0-send.sh
